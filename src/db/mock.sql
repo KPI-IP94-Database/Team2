be7cab557c9cd118b21a8f35fbcc7d522c301898
Destaby <rudik.nick.nick@gmail.com>
2020-11-30 12:13:24 +0200

    Fix mock.sql

--- src/db/mock.sql
+++ src/db/mock.sql
@@ -1,9 +1,9 @@
 -- passwords
 -- kiril, nikita, vova, sasha, illya, nick
 
-INSERT INTO User(user_name, login, password) VALUES(Kiril, 'loginKiril', '$scrypt$N=32768,r=8,p=1,maxmem=67108864$Km5w3hSy9GTOo0VnQ/FuVKTxzn0l8/5fw4kInHQqPiU$KhP5LNeJoGeEG/OyEnr2jGO7XKD7TnrI0nQsjgPATjtTEIHZ0e3YXeFBBGkdcMsZqhF7G/Vvhpv6ZP/2BL/HjQ');
-INSERT INTO User(user_name, login, password) VALUES(Nikita, 'loginNikita', '$scrypt$N=32768,r=8,p=1,maxmem=67108864$lt59T2ATUFPuC2UWtZpLJpUwfdgccQ3G0PvUI8UJpQU$SDWP2CLPDYAT0fh2tZ7gGp11N+KOvViLxbhrRpOLuZNfusaWAV21m0+cCfstoR/HnBmMFsJBJ/Xe2G4a6Db0fQ');
-INSERT INTO User(user_name, login, password) VALUES(Vova, 'loginVova', '$scrypt$N=32768,r=8,p=1,maxmem=67108864$clnzUntqp5wd0PTiXgwtZezRfa7KM2ZEFQZvvqt/QIM$C22iVpp8rg5FxhsNAURCYmz18Cv7F7abFLBoLGRdtKdNtSuoWSGJ4zVuaS1JXE9jJFs6d0hKt47LEnk5LnzJoA');
-INSERT INTO User(user_name, login, password) VALUES(Sasha, 'loginSasha', '$scrypt$N=32768,r=8,p=1,maxmem=67108864$NDkNEVIw9TmPEayLX8kflEJ83RbTNsyYbE8v/xTKvy4$SvSNfPKdFUvW1At975/cb8OxSEAEX4Jw9Jr6O/X/dRPANO32Hhi5fBgKfuexBSchSPQYJZ2anmadwfOguGHbig');
-INSERT INTO User(user_name, login, password) VALUES(Illya, 'loginIllya', '$scrypt$N=32768,r=8,p=1,maxmem=67108864$D2IgTweEtMNEuTJfccORsPOIGzRBIfgwthnW3t68s4s$J+bQDaV9YOJvApa7WH+Rf1wVFoyVavShe5C73FzKwMYmLIyod4vwPRnrXFhTak9/kbUvNwWHh6mae/XJc66uxA');
-INSERT INTO User(user_name, login, password) VALUES(Nick, 'loginNick', '$scrypt$N=32768,r=8,p=1,maxmem=67108864$6mUXwBJfc0xx9GMHDk8Lfq2mCysIrwU6zK3jIr837Bc$13btT87TbbHP5ABAQy4urlD+xKjresCHyYUgb0c6Pq7jeawXFG6+UcU6HoUYm8MocrAbHQ6h6i2HdQSAJ2b7hw');
+INSERT INTO User(name, login, password) VALUES(Kiril, 'loginKiril', '$scrypt$N=32768,r=8,p=1,maxmem=67108864$Km5w3hSy9GTOo0VnQ/FuVKTxzn0l8/5fw4kInHQqPiU$KhP5LNeJoGeEG/OyEnr2jGO7XKD7TnrI0nQsjgPATjtTEIHZ0e3YXeFBBGkdcMsZqhF7G/Vvhpv6ZP/2BL/HjQ');
+INSERT INTO User(name, login, password) VALUES(Nikita, 'loginNikita', '$scrypt$N=32768,r=8,p=1,maxmem=67108864$lt59T2ATUFPuC2UWtZpLJpUwfdgccQ3G0PvUI8UJpQU$SDWP2CLPDYAT0fh2tZ7gGp11N+KOvViLxbhrRpOLuZNfusaWAV21m0+cCfstoR/HnBmMFsJBJ/Xe2G4a6Db0fQ');
+INSERT INTO User(name, login, password) VALUES(Vova, 'loginVova', '$scrypt$N=32768,r=8,p=1,maxmem=67108864$clnzUntqp5wd0PTiXgwtZezRfa7KM2ZEFQZvvqt/QIM$C22iVpp8rg5FxhsNAURCYmz18Cv7F7abFLBoLGRdtKdNtSuoWSGJ4zVuaS1JXE9jJFs6d0hKt47LEnk5LnzJoA');
+INSERT INTO User(name, login, password) VALUES(Sasha, 'loginSasha', '$scrypt$N=32768,r=8,p=1,maxmem=67108864$NDkNEVIw9TmPEayLX8kflEJ83RbTNsyYbE8v/xTKvy4$SvSNfPKdFUvW1At975/cb8OxSEAEX4Jw9Jr6O/X/dRPANO32Hhi5fBgKfuexBSchSPQYJZ2anmadwfOguGHbig');
+INSERT INTO User(name, login, password) VALUES(Illya, 'loginIllya', '$scrypt$N=32768,r=8,p=1,maxmem=67108864$D2IgTweEtMNEuTJfccORsPOIGzRBIfgwthnW3t68s4s$J+bQDaV9YOJvApa7WH+Rf1wVFoyVavShe5C73FzKwMYmLIyod4vwPRnrXFhTak9/kbUvNwWHh6mae/XJc66uxA');
+INSERT INTO User(name, login, password) VALUES(Nick, 'loginNick', '$scrypt$N=32768,r=8,p=1,maxmem=67108864$6mUXwBJfc0xx9GMHDk8Lfq2mCysIrwU6zK3jIr837Bc$13btT87TbbHP5ABAQy4urlD+xKjresCHyYUgb0c6Pq7jeawXFG6+UcU6HoUYm8MocrAbHQ6h6i2HdQSAJ2b7hw');
